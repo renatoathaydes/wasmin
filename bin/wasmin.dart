753a62a3936b124735f1774e92ba6121b0137635
renatoathaydes <renatoathaydes@hotmail.com>
2020-01-03 22:50:21 +0100

    Better CLI implementation for testing.

--- bin/wasmin.dart
+++ bin/wasmin.dart
@@ -4,35 +4,43 @@ import 'package:args/args.dart';
 import 'package:wasmin/src/compile.dart';
 
 void main(List<String> args) async {
+  exit(await run(args));
+}
+
+Future<int> run(List<String> args) async {
   final argParser = ArgParser();
   argParser
-    ..addOption('output', abbr: 'o', help: 'output file')
+    ..addOption('output',
+        abbr: 'o', help: 'output file (if not given, prints to stdout)')
     ..addOption('runtime',
         allowed: {'wasmtime', 'wasmer'},
         defaultsTo: 'wasmtime',
         abbr: 't',
         help: 'the runtime to use to run programs')
     ..addFlag('help', abbr: 'h', help: 'show help message')
-    ..addFlag('run', abbr: 'r', help: 'run the program after compilation');
+    ..addFlag('run',
+        abbr: 'r',
+        help: 'run the program after compilation '
+            '(must compile to a file by using the -o option)');
 
   ArgResults parsedArgs;
   try {
     parsedArgs = argParser.parse(args);
   } catch (e) {
-    print(e.toString());
-    exit(1);
+    stderr.writeln('ERROR: ' + e.toString());
+    return 1;
   }
 
   if (parsedArgs.wasParsed('help')) {
     print('wasmin version 0.0.0\n\nUsage:\n${argParser.usage}');
-    exit(0);
+    return 0;
   }
 
   final inputs = parsedArgs.rest;
 
   if (inputs.isEmpty) {
-    print('At least one input file is required');
-    exit(1);
+    stderr.writeln('ERROR: At least one input file is required');
+    return 1;
   }
 
   final output = parsedArgs['output']?.toString();
@@ -41,18 +49,27 @@ void main(List<String> args) async {
 
   switch (result) {
     case CompilationResult.error:
-      print('There were compilation errors, see the compiler log for details');
-      exit(1);
-      break;
+      stderr.writeln('There were compilation errors, '
+          'see the compiler log for details');
+      return 1;
     case CompilationResult.success:
     // continue
   }
 
   if (parsedArgs.wasParsed('run')) {
-    final runtime = parsedArgs['runtime'].toString();
-    final result = await Process.run(runtime, [output]);
-    stdout.write(result.stdout);
-    stderr.write(result.stderr);
-    exit(result.exitCode);
+    if (output == null) {
+      stderr.writeln('ERROR: Cannot run program as it was not compiled to a '
+          'file.\nUse the -o option to compile the Wasmin program to a file.\n'
+          'See usage (wasmin -h) for details.');
+      return 2;
+    } else {
+      final runtime = parsedArgs['runtime'].toString();
+      final result = await Process.run(runtime, [output]);
+      stdout.write(result.stdout);
+      stderr.write(result.stderr);
+      return result.exitCode;
+    }
   }
+
+  return 0;
 }
