0075ed9ee4d917e681324325c1630db93bf31775
renatoathaydes <renatoathaydes@hotmail.com>
2020-01-04 16:44:14 +0100

    Added tasks to fully control build with Dartle.

--- dartle.dart
+++ dartle.dart
@@ -3,10 +3,91 @@ import 'dart:io';
 import 'package:dartle/dartle.dart';
 import 'package:path/path.dart' as path;
 
-Future<void> main(List<String> args) async {
-  final testProgramsDir = Directory('test/test_programs');
+FileFilter dartFileFilter = (f) => path.extension(f.path) == '.dart';
 
-  await run(args, tasks: {await WatCompiler(testProgramsDir).toTask()});
+final libDirDartFiles = dir('lib', fileFilter: dartFileFilter);
+final allDartFiles = dir('.', fileFilter: dartFileFilter);
+
+final checkImportsTask = Task(checkImports,
+    description: 'Checks dart file imports are allowed',
+    runCondition: RunOnChanges(inputs: libDirDartFiles));
+
+final formatCodeTask = Task(formatCode,
+    description: 'Formats all Dart source code',
+    runCondition: RunOnChanges(inputs: allDartFiles));
+
+final analyzeCodeTask = Task(analyzeCode,
+    description: 'Analyzes Dart source code',
+    runCondition: RunOnChanges(inputs: allDartFiles));
+
+final compileWatTask = WatCompiler(Directory('test/test_programs')).toTask();
+
+final testTask = Task(test,
+    description: 'Runs all tests. Arguments can be used to provide the '
+        'platforms the tests should run on.',
+    dependsOn: {analyzeCodeTask.name, compileWatTask.name},
+    argsValidator: const AcceptAnyArgs(),
+    runCondition:
+        RunOnChanges(inputs: dirs(const ['lib', 'bin', 'test', 'example'])));
+
+final verifyTask = Task((_) => null, // no action, just grouping other tasks
+    name: 'verify',
+    description: 'Verifies code style and linters, runs tests',
+    dependsOn: {'checkImports', 'formatCode', 'analyzeCode', 'test'});
+
+final cleanTask = Task(
+    (_) async =>
+        await ignoreExceptions(() => deleteOutputs({testTask, compileWatTask})),
+    name: 'clean',
+    description: 'Deletes the outputs of all other tasks');
+
+Future<void> main(List<String> args) async {
+  await run(args, tasks: {
+    cleanTask,
+    checkImportsTask,
+    formatCodeTask,
+    analyzeCodeTask,
+    compileWatTask,
+    testTask,
+    verifyTask,
+  }, defaultTasks: {
+    verifyTask
+  });
+}
+
+Future<void> test(List<String> platforms) async {
+  if (platforms.isEmpty) {
+    platforms = const ['chrome', 'vm'];
+  }
+  final platformArgs = platforms.expand((p) => ['-p', p]);
+  final code = await execProc(
+      Process.start('pub', ['run', 'test', ...platformArgs]),
+      name: 'Dart Tests');
+  if (code != 0) failBuild(reason: 'Tests failed');
+}
+
+Future<void> checkImports(List<String> _) async {
+  await for (final file in libDirDartFiles.files) {
+    final illegalImports = (await file.readAsLines()).where(
+        (line) => line.contains(RegExp("^import\\s+['\"]package:wasmin")));
+    if (illegalImports.isNotEmpty) {
+      failBuild(
+          reason: 'File ${file.path} contains '
+              'self import to the dartle package: ${illegalImports}');
+    }
+  }
+}
+
+Future<void> formatCode(List<String> _) async {
+  final code = await execProc(Process.start('dartfmt', const ['-w', '.']),
+      name: 'Dart Formatter');
+  if (code != 0) failBuild(reason: 'Dart Formatter failed');
+}
+
+Future<void> analyzeCode(List<String> _) async {
+  final code = await execProc(Process.start('dartanalyzer', const ['.']),
+      name: 'Dart Analyzer', successMode: StreamRedirectMode.stdout_stderr);
+  if (code != 0) failBuild(reason: 'Dart Analyzer failed');
 }
 
 class WatCompiler {
@@ -34,15 +115,15 @@ class WatCompiler {
     }
   }
 
-  Future<List<FileSystemEntity>> _watFiles() {
+  List<FileSystemEntity> _watFiles() {
     return testProgramsDir
-        .list()
+        .listSync()
         .where((entity) => path.extension(entity.path) == '.wat')
         .toList();
   }
 
-  Future<Task> toTask() async {
-    final watFiles = await _watFiles();
+  Task toTask() {
+    final watFiles = _watFiles();
     final testWasmFiles = watFiles
         .map((entity) => path.setExtension(entity.path, '.wasm'))
         .toList();
